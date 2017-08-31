60868d57fb0dd606f4d1ab24762958bd70d38341
mehdi <mehdi@stash.pm>
2017-08-31 17:38:27 +0200

    WIP: build process

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      sourceMap: true
+    })
+  ]
+}
