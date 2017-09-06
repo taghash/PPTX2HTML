a067482240f5bfb60bfc2c16f14bec9fa3fea78b
mehdi <mehdi@stash.pm>
2017-09-06 14:23:06 +0200

    WIP: adding builtins & globals

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import builtins from 'rollup-plugin-node-builtins'
+import nodeGlobals from 'rollup-plugin-node-globals'
 
 export default {
   plugins: [
+    nodeGlobals(),
+    builtins(),
     nodeResolve({
       jsnext: true,
       main: true
